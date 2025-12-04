ba4c61515d0baeccb00cd50c9326cd4c64035027
Federico <Fedealtrui@gmail.com>
2025-12-04 15:41:34 -0300

    Final Build 1.0

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'Omakase-love';
+
+const nextConfig = {
+  output: 'export',
+
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
